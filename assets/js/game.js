7e7fe3831510640909367ba5d42cb0032590d25b
gregoryjohncarter <gregoryjohncarter@gmail.com>
2021-11-29 13:42:42 -0500

    fix multiple opponents

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,8 +10,8 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
@@ -59,14 +59,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
   }
 }
-/*
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
 }
-*/
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
+
 // fight();
