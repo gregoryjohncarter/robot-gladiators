86d8be6b63b0797db265b33ffc51b2da8e29fd4d
gregoryjohncarter <gregoryjohncarter@gmail.com>
2021-11-30 13:58:11 -0500

    fix blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,6 +77,7 @@ var startGame = function() {
 for (var i = 0; i < enemyInfo.length; i++) {
   if (playerInfo.health > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+    
   
   var pickedEnemyObj = enemyInfo[i];
   pickedEnemyObj.health = randomNumber(40, 60);
@@ -145,6 +146,7 @@ switch (shopOptionPrompt) {
   playerInfo.upgradeAttack();
     break;
   case "leave":
+    case "LEAVE":
     window.alert("Leaving the store.");
 
     // do nothing, so function will end
@@ -158,10 +160,19 @@ switch (shopOptionPrompt) {
 }
 };
 
+var getPlayerName = function() {
+  var name = "";
 
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
 
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
